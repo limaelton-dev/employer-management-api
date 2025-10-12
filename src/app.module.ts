bd05ff82747586b1b95862c88b7710acbad89327
Elton Lima <eltonlima.contato@gmail.com>
2025-10-12 11:18:19 -0300

    create db and main entities with seeds with sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Employee } from './entities/employee.entity';
+import { ContactInfo } from './entities/contact-info.entity';
+import { Task } from './entities/task.entity';
+import { Meeting } from './entities/meeting.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logging: true,
+    }),
+    TypeOrmModule.forFeature([
+      Employee,
+      ContactInfo,
+      Task,
+      Meeting,
+    ]),
+  ],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,62 @@
 import { Injectable } from '@nestjs/common';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { Employee } from './entities/employee.entity';
+import { ContactInfo } from './entities/contact-info.entity';
+import { Task } from './entities/task.entity';
+import { Meeting } from './entities/meeting.entity';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectDataSource()
+    private readonly dataSource: DataSource
+  ) {}
+
+  async seed() {
+    await this.dataSource.transaction(async (db) => {
+      const ceo = db.create(Employee, {
+        name: 'Mr Jones',
+      })
+
+      await db.save(ceo);
+
+      const contactInfo = db.create(ContactInfo, {
+        email: 'mrjones@email.com',
+        employee: ceo,
+      })
+
+      await db.save(contactInfo)
+
+      const manager = db.create(Employee, {
+        name: 'Manager',
+        manager: ceo,
+      })
+
+      await db.save(manager)
+
+      const task1 = db.create(Task, {
+        name: 'Hire People',
+        assignee: manager,
+      })
+
+      const task2 = db.create(Task, {
+        name: 'Present to CEO',
+        assignee: manager,
+      })
+
+      await db.save([task1, task2])
+
+      const meeting = db.create(Meeting, {
+        attendees: [ceo],
+        zoomUrl: 'https://zoom.us/123',
+      })
+
+      await db.save(meeting)
+
+      meeting.attendees = [ceo, manager]
+
+      await db.save(meeting)
+    })
   }
 }
